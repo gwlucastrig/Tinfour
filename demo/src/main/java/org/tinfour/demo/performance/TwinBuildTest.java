5380db49cc5d5965dcb2b0177d9f6969dc55b591
gwlucastrig <contact.tinfour@gmail.com>
2019-01-29 20:17:51 -0500

    Adjustments for Maven Builds

--- demo/src/main/java/org/tinfour/demo/performance/TwinBuildTest.java
+++ demo/src/main/java/org/tinfour/demo/performance/TwinBuildTest.java
@@ -58,8 +58,8 @@ public class TwinBuildTest implements IDevelopmentTest {
 
   // run test against self to establish that there is no bias
   // depending on the order in which builds are performed.
-  static final String defaultClassA = "tinfour.standard.IncrementalTin";
-  static final String defaultClassB =  "tinfour.semivirtual.SemiVirtualIncrementalTin";
+  static final String defaultClassA = "org.tinfour.standard.IncrementalTin";
+  static final String defaultClassB = "org.tinfour.semivirtual.SemiVirtualIncrementalTin";
 
   /**
    * Perform a simple test of the TIN building functions a fixed number of
