35ce32457d3ac78cedbd9b6dfbf5ec35946a1244
gwlucastrig <contact.tinfour@gmail.com>
2019-08-14 08:34:52 -0400

    Javadoc updates for contouring

--- core/src/main/java/org/tinfour/contour/ContourRegion.java
+++ core/src/main/java/org/tinfour/contour/ContourRegion.java
@@ -29,11 +29,14 @@
  */
 package org.tinfour.contour;
 
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Path2D;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
+ * Provides a elements and access methods for a region created
+ * through a contour-building process.
  */
 public class ContourRegion {
 
@@ -45,10 +48,17 @@ public class ContourRegion {
   ContourRegion parent;
   List<ContourRegion> children = new ArrayList<>();
 
+  /**
+   * Standard constructor
+   */
   ContourRegion() {
 
   }
 
+  /**
+   * Construct a region based on a single, closed-loop contour.
+   * @param contour a valid instance describing a single, closed-loop contour.
+   */
   ContourRegion(Contour contour) {
     assert contour.closedLoop : "Single contour constructor requires closed loop";
     if (contour.closedLoop) {
@@ -75,6 +85,10 @@ public class ContourRegion {
   }
 
   
+  /**
+   * Get the XY coordinates for the contour region
+   * @return a safe copy of the geometry of the contour region.
+   */
   public double[] getXY() {
     Contour contour = memberList.get(0).contour;
     return contour.getCoordinates();
@@ -85,6 +99,12 @@ public class ContourRegion {
     region.parent = this;
   }
 
+  /**
+   * Indicates whether the specified point is inside the region
+   * @param x the Cartesian coordinate for the point of interest
+   * @param y the Cartesian coordinate for the point of interest
+   * @return true if the point is inside the contour; otherwise, fakse
+   */
   public boolean isPointInsideRegion(double x, double y) {
     Contour c = memberList.get(0).contour;
     double[] xy = c.xy;
@@ -122,11 +142,75 @@ public class ContourRegion {
       return true; // unambiguously inside
     }
     return false; // unambiguously outside
-
+  }
+  
+  /**
+   * Gets the absolute value of the area of the region.
+   * @return a positive value, potentially zero if the region is
+   * incompletely specified.
+   */
+  public double getAbsoluteArea(){
+    return absArea;
   }
 
+  /**
+   * Gets the signed area of the region. If the points that specify the
+   * region are given in a counter-clockwise order, the region will have
+   * a positive area. If the points are given in a clockwise order,
+   * the region will have a negative area.
+   * @return 
+   */
+  public double getSignedArea(){
+    return area;
+  }
+  
+  /**
+   * Gets the index of the region. The indexing scheme is based on
+   * the original values of the zContour array used when the contour
+   * regions were built. The minimim proper region index is zero.
+   * <p>
+   * At this time, regions are not constructed for areas of null
+   * data. In future implementations, null-data regions will be 
+   * indicated by a region index of -1.
+   * @return a positive integer value, or -1 for null-data regions.
+   */
+  public int getRegionIndex(){
+    return regionIndex;
+  }
+  
+  /**
+   * Gets a Path2D suitable for rendering purposes.
+   *
+   * @param transform a valid AffineTransform, typically specified to map the
+   * Cartesian coordinates of the contour to pixel coordinate.
+   * @return a valid instance
+   */
+  public Path2D getPath2D(AffineTransform transform) {
+    double[] xy = getXY();
+    int n = xy.length;
+    Path2D path = new Path2D.Double();
+    if (n >= 4) {
+      double[] c = new double[n];
+      transform.transform(xy, 0, c, 0, n / 2);
+
+      path.moveTo(c[0], c[1]);
+      for (int i = 1; i < n / 2; i++) {
+        path.lineTo(c[i * 2], c[i * 2 + 1]);
+      }
+      path.closePath();
+
+    }
+    return path;
+  }
+
+  
+  
+  
+  
   @Override
   public String toString() {
-    return String.format("%4d %12.2f  %s %d", regionIndex, area, parent == null ? "root " : "child", children.size());
+    return String.format("%4d %12.2f  %s %d",
+            regionIndex, area, parent == null ? "root " : "child", 
+            children.size());
   }
 }
